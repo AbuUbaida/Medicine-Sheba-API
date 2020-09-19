723b4b50ba3baca82cc432a8c190ae0f3bc08c13
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2020-09-19 21:27:24 +0600

    upadte-medicine router added

--- src/routers/admin/medicine.js
+++ src/routers/admin/medicine.js
@@ -36,20 +36,28 @@ router.post('/admin/add-medicine', async (req, res) => {
 })
 
 
-// router.post('/admin/update-medicine', async (req, res) => {
-//     try {
-//         const registeredMedicine = await Medicine.findByIdAndUpdate(req.body._id,{})
-//         res.status(201).json({ status: 'success', message: registeredMedicine })
-//     } catch (e) {
-//         res.status(400).json({ status: 'error', message: e.message })
-//     }
-// })
+router.post('/admin/update-medicine', async (req, res) => {
+    try {
+        const registeredMedicine = await Medicine.findByIdAndUpdate(req.body._id,
+            {
+                medicineName:req.body.medicineName,
+                strength:req.body.strength,
+                strength:req.body.strength,
+                genericName:req.body.genericName,
+                manufacturer:req.body.manufacturer,
+                price:req.body.price
+            })
+        res.status(201).json({ status: 'success', message: registeredMedicine })
+    } catch (e) {
+        res.status(400).json({ status: 'error', message: e.message })
+    }
+})
 
 
 router.post('/admin/remove-medicine', async (req, res) => {
     try {
         const registeredMedicine = await Medicine.findByIdAndDelete(req.body._id)
-        res.status(201).json({ status: 'success', message: registeredMedicine })
+        res.status(200).json({ status: 'success', message: registeredMedicine })
     } catch (e) {
         res.status(400).json({ status: 'error', message: e.message })
     }
