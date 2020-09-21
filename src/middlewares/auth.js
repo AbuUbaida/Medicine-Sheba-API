7cb239ba00287a40d6a6f3441e8fd2211143dc2a
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2020-09-21 15:34:48 +0600

    auth middleware modified

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -15,7 +15,7 @@ const auth = async (req, res, next) => {
         req.user = user
         next()
     } catch (e) {
-        res.status(401).json({ error: 'Please authenticate' })
+        res.status(401).json({ error: e})
     }
 }
 
