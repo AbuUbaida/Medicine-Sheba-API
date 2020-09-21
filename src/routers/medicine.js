f625be8589c5f0bfeebf84052f3dfdeda3ef5b85
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2020-09-21 23:57:37 +0600

    error solved

--- src/routers/medicine.js
+++ src/routers/medicine.js
@@ -8,6 +8,11 @@ const router = new express.Router()
 router.get('/medicines', auth, async (req, res) => {
     try {
         const medicines = await Medicine.find() //find all the medicines
+        
+        if (medicines.length == 0) {
+            return res.status(404).json({ status: 'error', message: 'You do not have any medicine' })
+        }
+        
         res.status(200).json({ status: 'success', message: medicines })
     } catch (e) {
         res.status(400).json({ status: 'error', message: e.message })
