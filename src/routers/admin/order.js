f2177e9556c4aff3471b8e797c157cca6330cb14
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2020-09-21 22:10:42 +0600

    admin auth modified

--- src/routers/admin/order.js
+++ src/routers/admin/order.js
@@ -4,11 +4,13 @@ const auth = require('../../middlewares/authAdmin')
 const Order = require('../../models/order')
 const router = new express.Router()
 
-
+//show all the orders those are still pending to the admin
 router.get('/admin/orders/pending', auth, async (req, res) => {
     try {
-        const orders = await Order.find({ orderStatus: 'Pending' }).populate('owner').exec()
+        const orders = await Order.find({ orderStatus: 'Pending' }).populate('owner').exec() //finds
+        // the pending orders from documents and populate the document by it's owner field;
 
+        //if the retrieved orders length is zero that means no such order is available
         if (orders.length == 0) {
             return res.status(404).json({ status: 'error', message: 'No pending order available' })
         }
@@ -22,7 +24,7 @@ router.get('/admin/orders/pending', auth, async (req, res) => {
             const dateTime = moment(order.createdAt).format('DD/MM/YYYY hh:mm a')
             const subTotal = order.subTotal
             const obj = { orderId, ownerId, orderNo, customerName, dateTime, subTotal }
-            response.push(obj)
+            response.push(obj) //manipulating the response data
         })
 
         res.status(200).json({ status: 'success', message: response })
@@ -32,6 +34,7 @@ router.get('/admin/orders/pending', auth, async (req, res) => {
 })
 
 
+//show all the orders those are still accepted to the admin
 router.get('/admin/orders/accepted', auth, async (req, res) => {
     try {
         const orders = await Order.find({ orderStatus: 'Accepted' }).populate('owner').exec()
@@ -59,6 +62,7 @@ router.get('/admin/orders/accepted', auth, async (req, res) => {
 })
 
 
+//show all the orders those are still delivered to the admin
 router.get('/admin/orders/delivered', auth, async (req, res) => {
     try {
         const orders = await Order.find({ orderStatus: 'Delivered' }).populate('owner').exec()
@@ -86,13 +90,14 @@ router.get('/admin/orders/delivered', auth, async (req, res) => {
 })
 
 
-
-
-//need to change; find owner:......
+//get the details of a selected order that admin has
 router.get('/admin/orders/:orderNo', auth, async (req, res) => {
     try {
-        const order = await Order.findOne({ orderNo: req.params.orderNo }).populate('owner medicineId').exec()
+        const order = await Order.findOne({ orderNo: req.params.orderNo })
+            .populate('owner medicineId').exec() //populating multiple fields here (owner medicineId)
+        // to access the data of other collection
 
+        //if no order is returned means no such order found
         if (!order) {
             res.status(404).json({ status: 'error', message: 'Order not found' })
         }
@@ -103,7 +108,8 @@ router.get('/admin/orders/:orderNo', auth, async (req, res) => {
         const medicineDetails = order.orderDetails
         const dateTime = moment(order.createdAt).format('DD/MM/YYYY hh:mm a')
         const subTotal = order.subTotal
-        const response = { orderNo, customerName, medicineDetails, dateTime, subTotal }
+        const response = { orderNo, customerName, medicineDetails, dateTime, subTotal } //manipulates 
+        //the response data
 
         res.status(200).json({ status: 'success', message: response })
     } catch (e) {
@@ -112,15 +118,17 @@ router.get('/admin/orders/:orderNo', auth, async (req, res) => {
 })
 
 
+//used to change the status of an order; like from Accepted to Delivered or from Pending to Accepted
 router.post('/admin/orders/change-status', auth, async (req, res) => {
     try {
         const order = await Order.findOne({ orderNo: req.body.orderNo, orderStatus: req.body.statusFrom })
 
+        //if no order is returned means no such order found
         if (!order) {
             res.status(404).json({ status: 'error', message: 'Order not found' })
         }
 
-        order.orderStatus = req.body.statusTo
+        order.orderStatus = req.body.statusTo //sets the new status admin wants to do
         order.save()
         res.status(200).json({ status: 'success', message: 'Order status changed' })
     } catch (e) {
